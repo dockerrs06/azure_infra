a2330ddd284e9f834c0306a34c28e57739946cb7
dockerrs06 <dockerss06@gmail.com>
2026-05-21 15:00:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,40 @@
+
+# ----------------------------
+# Resource Group
+# ----------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "isac-${var.env}-rg"
+  location = var.location
+}
+
+# ----------------------------
+# Storage Account
+# ----------------------------
+resource "azurerm_storage_account" "sa" {
+  name                     = lower("isac${var.env}sa${random_string.rand.result}")
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.env
+  }
+}
+
+# Random for unique SA name
+resource "random_string" "rand" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
+
+
+
+
+
 module "windows_vm" {
   source = "./modules/virtualmachine"
 
