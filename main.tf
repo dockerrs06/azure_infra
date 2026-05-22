8a7311973bd182fb73221086c41499a1f2cfb9e9
dockerrs06 <dockerss06@gmail.com>
2026-05-22 10:35:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = each.value
+  count_replication_type = each.value.replication_type
 
   tags = {
     environment = var.env
