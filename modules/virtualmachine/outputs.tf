8363ad746b72bfdff35f814a69303e696a5755e8
dockerrs06 <dockerss06@gmail.com>
2026-05-22 10:19:58 +0530

    Update outputs.tf

--- modules/virtualmachine/outputs.tf
+++ modules/virtualmachine/outputs.tf
@@ -1,7 +1,4 @@
-output "vm_name" {
-  value = azurerm_windows_virtual_machine.vm.name
-}
 
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
+output "vm_names" {
+  value = azurerm_windows_virtual_machine.vm.name
 }
