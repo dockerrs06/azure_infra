bcedd88267c7c3b17360c0b5396e1e0adfefab22
dockerrs06 <dockerss06@gmail.com>
2026-05-22 10:39:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,11 @@
 variable "env" {}
 variable "location" {}
 
-variable "admin_username" {}
-variable "admin_password" {
-  sensitive = true
-}
-
-# ✅ VM MAP
 variable "vm_config" {
   type = map(object({
     size = string
   }))
 }
-
-# ✅ Storage MAP
-#v ariable "storage_config" {
-  # type = map(string)
-# }
 
 variable "storage_config" {
   type = map(object({
@@ -24,3 +13,7 @@ variable "storage_config" {
   }))
 }
 
+variable "admin_username" {}
+variable "admin_password" {
+  sensitive = true
+}
