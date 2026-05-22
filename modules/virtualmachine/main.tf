7abb8cfefe9a9d788a277218205a36b7dcbdf9a9
dockerrs06 <dockerss06@gmail.com>
2026-05-22 10:19:37 +0530

    Update main.tf

--- modules/virtualmachine/main.tf
+++ modules/virtualmachine/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.env}-${var.vm_name}-vnet"
-  address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -17,24 +17,6 @@ resource "azurerm_public_ip" "pip" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.env}-${var.vm_name}-nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  security_rule {
-    name                       = "RDP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -52,9 +34,9 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "${var.env}-${var.vm_name}"
-  resource_group_name = var.resource_group_name
   location            = var.location
-  size                = "Standard_B2s"
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
 
   admin_username = var.admin_username
   admin_password = var.admin_password
