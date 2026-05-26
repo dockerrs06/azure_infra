9990d1145240c2c122b01dfcb8babd9bd3eabbc7
dockerrs06 <dockerss06@gmail.com>
2026-05-26 22:17:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,48 +7,12 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # =========================
-# Random String (Storage)
-# =========================
-resource "random_string" "rand" {
-  length  = 4
-  special = false
-  upper   = false
-}
-
-# =========================
-# Storage Account
-# =========================
-resource "azurerm_storage_account" "sa" {
-  name                     = lower("isac${var.env}sa${random_string.rand.result}")
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = var.env
-  }
-}
-
-# =========================
-# Virtual Machine Module
-# =========================
-module "windows_vm" {
-  source = "./modules/virtualmachine"
-
-  env                 = var.env
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  vm_name             = var.vm_name
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-}
-
-# =========================
-# AKS (Azure Kubernetes Service) Module
+# AKS MODULE
 # =========================
 module "aks" {
   source = "./modules/kubernetes"
+
+  count = var.enable_aks ? 1 : 0
 
   cluster_name        = "isac-${var.env}-aks"
   location            = azurerm_resource_group.rg.location
@@ -62,4 +26,20 @@ module "aks" {
   tags = {
     environment = var.env
   }
+}
+
+# =========================
+# VM MODULE
+# =========================
+module "windows_vm" {
+  source = "./modules/virtualmachine"
+
+  count = var.enable_vm ? 1 : 0
+
+  env                 = var.env
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  vm_name             = var.vm_name
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
 }
