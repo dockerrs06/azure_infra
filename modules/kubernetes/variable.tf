37cbc98a2c63f986ac537172ef235bc4a3c78479
dockerrs06 <dockerss06@gmail.com>
2026-05-26 21:56:39 +0530

    Update variable.tf

--- modules/kubernetes/variable.tf
+++ modules/kubernetes/variable.tf
@@ -1,1 +1,36 @@
 
+variable "cluster_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "node_pool_name" {
+  type    = string
+  default = "default"
+}
+
+variable "node_count" {
+  type    = number
+  default = 1
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_D2_v2"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
